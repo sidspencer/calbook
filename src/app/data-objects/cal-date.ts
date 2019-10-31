7acb1cbc8c12b6b932fad85ca108390f4c1d72f3
sidspencer <sidspencer@gmail.com>
2019-10-31 16:37:48 -0500

    Return default dates on failed construction.

--- src/app/data-objects/cal-date.ts
+++ src/app/data-objects/cal-date.ts
@@ -3,12 +3,14 @@ import { NumberUtil } from '../util-objects/number-util';
 export class CalDate {
     public dd: number = 1;
     public mm: number = 1;
-    public yyyy: number = 2000;
+    public yyyy: number = 2001;
 
     constructor(dateCode: string) {
-        this.yyyy = parseInt(dateCode.substring(0, 4), 10);
-        this.mm = parseInt(dateCode.substring(4, 6), 10);
-        this.dd = parseInt(dateCode.substring(6), 10);
+        if (!!dateCode) {
+            this.yyyy = parseInt(dateCode.substring(0, 4), 10);
+            this.mm = parseInt(dateCode.substring(4, 6), 10);
+            this.dd = parseInt(dateCode.substring(6), 10);
+        }
     }
 
     public toDateCode(): string {
@@ -17,6 +19,5 @@ export class CalDate {
 
     public toUsaDateFormat(): string {
         return NumberUtil.toUsaDateFormat(this);
-
     }
 }
