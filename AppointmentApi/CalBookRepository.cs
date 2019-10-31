8e3a0dfb55026e2954a8ed7bb019c9bba39b733c
sidspencer <sidspencer@gmail.com>
2019-10-31 16:31:12 -0500

    Fix incorrect old code references.

--- AppointmentApi/CalBookRepository.cs
+++ AppointmentApi/CalBookRepository.cs
@@ -22,9 +22,9 @@ namespace CalBookApi
                             .ToListAsync();
         }
 
-        public Task<Appointment> GetAppointment(string name)
+        public Task<Appointment> GetAppointment(ObjectId id)
         {
-            FilterDefinition<Appointment> filter = Builders<Appointment>.Filter.Eq(m => m.notes, name);
+            FilterDefinition<Appointment> filter = Builders<Appointment>.Filter.Eq(m => m.id, id);
 
             return _context
                     .Appointments
@@ -32,27 +32,27 @@ namespace CalBookApi
                     .FirstOrDefaultAsync();
         }
        
-        public async Task CreateAppointment(Appointment game)
+        public async Task CreateAppointment(Appointment appointment)
         {
-            await _context.Appointments.InsertOneAsync(game);
+            await _context.Appointments.InsertOneAsync(appointment);
         }
 
-        public async Task<bool> UpdateAppointment(Appointment game)
+        public async Task<bool> UpdateAppointment(Appointment appointment)
         {
             ReplaceOneResult updateResult =
                 await _context
                         .Appointments
                         .ReplaceOneAsync(
-                            filter: g => g.id == game.id,
-                            replacement: game);
+                            filter: g => g.id == appointment.id,
+                            replacement: appointment);
 
             return updateResult.IsAcknowledged
                     && updateResult.ModifiedCount > 0;
         }
 
-        public async Task<bool> DeleteAppointment(string name)
+        public async Task<bool> DeleteAppointment(ObjectId id)
         {
-            FilterDefinition<Appointment> filter = Builders<Appointment>.Filter.Eq(m => m.notes, name);
+            FilterDefinition<Appointment> filter = Builders<Appointment>.Filter.Eq(m => m.id, id);
 
             DeleteResult deleteResult = await _context
                                                 .Appointments
@@ -64,7 +64,7 @@ namespace CalBookApi
 
         public async Task<IEnumerable<Appointment>> GetAppointmentsByDate(string dateCode) {
             CalDate cd = new CalDate(dateCode);
-           var f2 = "{ $and: [{\"calDate.yyyy\": " + cd.yyyy + " }, {\"calDate.mm\": " + cd.mm + "}, {\"calDate.dd\": " + cd.dd + "}] }";
+            var f2 = "{ $and: [{\"calDate.yyyy\": " + cd.yyyy + " }, {\"calDate.mm\": " + cd.mm + "}, {\"calDate.dd\": " + cd.dd + "}] }";
 
             return await _context
                     .Appointments
