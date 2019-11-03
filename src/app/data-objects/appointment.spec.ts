d1b743cd37448701da208347cc9da556ef00da09
sidspencer <sidspencer@gmail.com>
2019-11-03 17:20:43 -0600

    Change Id to be a string

--- src/app/data-objects/appointment.spec.ts
+++ src/app/data-objects/appointment.spec.ts
@@ -1,10 +1,9 @@
 import { Appointment } from './appointment';
 import { CalDate } from './cal-date';
 import { Timeslot } from './timeslot';
-import { ObjectId } from './object-id';
 
 describe('Appointment', () => {
   it('should create an instance', () => {
-    expect(new Appointment(new ObjectId('0', 0, 0, 0, 0), new CalDate('20010101'), new Timeslot(1), '')).toBeTruthy();
+    expect(new Appointment('23423442342234', new CalDate('20010101'), new Timeslot(1), '')).toBeTruthy();
   });
 });
--- src/app/data-objects/appointment.ts
+++ src/app/data-objects/appointment.ts
@@ -1,17 +1,14 @@
 import { CalDate } from './cal-date';
 import { Timeslot } from './timeslot';
-import { ObjectId } from './object-id';
 
 export class Appointment {
-    public Id: string;
-    public id: ObjectId;
+    public id: string;
     public calDate: CalDate;
     public timeslot: Timeslot;
     public notes: string;
 
-    constructor(id: ObjectId, calDate: CalDate, timeslot: Timeslot, notes: string) {
+    constructor(id: string, calDate: CalDate, timeslot: Timeslot, notes: string) {
         this.id = id;
-        this.Id = JSON.stringify(id);
         this.calDate = calDate;
         this.timeslot = timeslot;
         this.notes = notes;
