717935e3f074548a6b4ad8a0c81f13592a6fffbf
sidspencer <sidspencer@gmail.com>
2019-11-04 15:11:45 -0600

    Change to using node server

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -4,29 +4,32 @@ import { HttpClient } from '@angular/common/http';
 import { CalDate } from '../data-objects/cal-date';
 import { Appointment } from '../data-objects/appointment';
 
+const baseUrl = 'http://localhost:3003';
+//const baseUrl = 'http://localhost:5000';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
   deleteAppointment(a: Appointment): Observable<any> {
-    return (this.http.delete(`http://localhost:5000/api/appointment/${a.id}`) as Observable<any>);
+    return (this.http.delete(`${baseUrl}/api/appointment/${a.id}`) as Observable<any>);
   }
 
   constructor(protected http: HttpClient) { }
 
   public fetchAppointmentsByDate(date: CalDate): Observable<Appointment[]> {
-    return (this.http.get(`http://localhost:5000/api/appointment/bydate/${date.toDateCode()}`) as Observable<Appointment[]>);
+    return (this.http.get(`${baseUrl}/api/appointment/bydate/${date.toDateCode()}`) as Observable<Appointment[]>);
   }
 
   public fetchAllAppointments(): Observable<Appointment[]> {
-    return (this.http.get('http://localhost:5000/api/appointment')) as Observable<Appointment[]>;
+    return (this.http.get(`${baseUrl}/api/appointment`)) as Observable<Appointment[]>;
   }
 
   public createAppointment(a: Appointment): Observable<any> {
-    return (this.http.put('http://localhost:5000/api/appointment', a) as Observable<any>);
+    return (this.http.put(`${baseUrl}/api/appointment`, a) as Observable<any>);
   }
 
   public updateAppointment(a: Appointment): Observable<any> {
-    return (this.http.post(`http://localhost:5000/api/appointment/`, a) as Observable<Appointment>);
+    return (this.http.post(`${baseUrl}/api/appointment/`, a) as Observable<Appointment>);
   }
 }
