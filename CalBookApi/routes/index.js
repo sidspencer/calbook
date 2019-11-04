ad1ad523407fe8c3fc5687ab0ff45405ce3e97b8
sidspencer <sidspencer@gmail.com>
2019-11-04 17:29:20 -0600

    Cleanup. Also parse schedule date in get by date controller.

--- CalBookApi/routes/index.js
+++ CalBookApi/routes/index.js
@@ -4,21 +4,17 @@ var CalBookRepo = require('../src/CalBookRepo');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   CalBookRepo.getAllAppointments().then((appointments) => {
     res.send(appointments);
   });
 });
 
 router.get('/bydate/:dateCode', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   let dc = req.params['dateCode'];
   if (!!dc && !!dc.length) {
-    let yyyy = dc.substring(0,4);
-    let mm = dc.substring(4,6);
-    let dd = dc.substring(6);
+    let yyyy = Number.parseInt(dc.substring(0,4));
+    let mm = Number.parseInt(dc.substring(4,6));
+    let dd = Number.parseInt(dc.substring(6));
 
     CalBookRepo.getAppointmentsByDate(yyyy, mm, dd).then((result) => {
       res.send(result);
@@ -30,8 +26,6 @@ router.get('/bydate/:dateCode', (req, res, next) => {
 
 // create
 router.put('/', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   CalBookRepo.createNewAppointment(req.body).then((result) => {
     res.send(result);
   });
@@ -39,15 +33,11 @@ router.put('/', (req, res, next) => {
 
 //update
 router.post('/', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   CalBookRepo.updateAppointment(req.body).then((result) => {
     res.send(result);
   });});
 
 router.delete('/:id', (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   CalBookRepo.deleteAppointment(req.params['id']).then((result) => {
     res.send(result);
   });});
