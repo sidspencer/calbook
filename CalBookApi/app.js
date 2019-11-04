a291267b0dc50d3dc0e490e4394a536edfbf2f48
sidspencer <sidspencer@gmail.com>
2019-11-04 17:28:46 -0600

    Open up for CORS

--- CalBookApi/app.js
+++ CalBookApi/app.js
@@ -10,6 +10,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/appointment', indexRouter);
 
 // catch 404 and forward to error handler
